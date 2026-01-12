1c91ae1ac5c784817d2e6138ed15fa492e8fe909
Aditya  Shivshankar kushwaha <adityakuashwaha@gmail.com>
2026-01-12 22:20:37 +0530

    Commit
    Commit into main

---
+++ src/main/java/models/user.java
@@ -0,0 +1,19 @@
+package models;
+
+public class user {
+    private int userID;
+    private String name;
+    private int email;
+    private int phone;
+
+    public user(int userID,String name,int email,int phone){
+        this.userID=userID;
+        this.name=name;
+        this.email=email;
+        this.phone=phone;
+        
+
+    }
+
+	
+}
